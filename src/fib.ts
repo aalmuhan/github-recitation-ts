a6a4321d4a4150f3d3fbe7bbf4d8a716f2ca209e
aisha <aalmuhan@andrew.cmu.edu>
2025-08-31 16:19:46 +0300

    Fix issue #2 type-safe: Fibonacci function in fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,10 @@
 // util function that computes the Fibonacci numbers
+
 export function fibonacci(n: number): number {
-  if (n < 0) {
-    return -1; // invalid input
-  } else if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
-  }
+  if (n < 0) return -1;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
